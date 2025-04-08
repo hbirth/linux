376762e2282d1d08cb067271c2558d057dbc17ed
Bernd Schubert <bschubert@ddn.com>
2025-04-08 16:44:55 +0200

    fuse: Increase the default max pages limit to 8182
    Due to user buffer misalignent we actually need one page more,
    i.e. 1025 instead of 1024, will be handled differently.
    For now we just bump up the max.
    
    (imported from commit 3f71501c9c4702ba976145ff15c4a053ecd1a3ee)

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -37,7 +37,8 @@ DEFINE_MUTEX(fuse_mutex);
 
 static int set_global_limit(const char *val, const struct kernel_param *kp);
 
-unsigned int fuse_max_pages_limit = 256;
+unsigned int fuse_max_pages_limit = 4097;
+
 /* default is no timeout */
 unsigned int fuse_default_req_timeout;
 unsigned int fuse_max_req_timeout;
