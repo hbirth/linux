76dce1ef10eade6e6b876fdcc62af60d4a1ff7b6
Bernd Schubert <bschubert@ddn.com>
2025-06-04 19:32:39 +0200

    fuse: {io-uring} Allow reduced number of ring queues
    Queues selection (fuse_uring_get_queue) can handle reduced number
    queues - using io-uring is possible now even with a single
    queue and entry.
    
    The FUSE_URING_REDUCED_Q flag is being introduce tell fuse server that
    reduced queues are possible, i.e. if the flag is set, fuse server
    is free to reduce number queues.
    
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>
    
    (imported from commit f620f3d35969bd9a04304b757a18a11a0787dedc)

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -450,6 +450,8 @@ struct fuse_file_lock {
  *			 init_out.request_timeout contains the timeout (in secs)
  * FUSE_INVAL_INODE_ENTRY: invalidate inode aliases when doing inode invalidation
  * FUSE_EXPIRE_INODE_ENTRY: expire inode aliases when doing inode invalidation
+ * FUSE_URING_REDUCED_Q: Client (kernel) supports less queues - Server is free
+ *			 to register between 1 and nr-core io-uring queues
  */
 #define FUSE_ASYNC_READ		(1 << 0)
 #define FUSE_POSIX_LOCKS	(1 << 1)
@@ -497,6 +499,8 @@ struct fuse_file_lock {
 #define FUSE_ALLOW_IDMAP	(1ULL << 40)
 #define FUSE_OVER_IO_URING	(1ULL << 41)
 #define FUSE_REQUEST_TIMEOUT	(1ULL << 42)
+#define FUSE_ALIGN_PG_ORDER	(1ULL << 50)
+#define FUSE_URING_REDUCED_Q	(1ULL << 59)
 #define FUSE_INVAL_INODE_ENTRY  (1ULL << 60)
 #define FUSE_EXPIRE_INODE_ENTRY (1ULL << 61)
 
