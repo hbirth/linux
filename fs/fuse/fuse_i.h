52e9135ada4992e71a4ffca91b12d6bc44b704e2
Bernd Schubert <bschubert@ddn.com>
2025-06-20 17:34:53 +0200

    fuse: add DLM_LOCK opcode
    When having writeback cache enabled it is beneficial for data consistency
    to communicate to the FUSE server when the kernel prepares a page for caching.
    This lets the FUSE server react and lock the page.
    
    Additionally the kernel lets the FUSE server decide how much data it locks by the
    same call and keeps the given information in the dlm lock management.
    
    If the feature is not supported it will be disabled after first unsuccessful use.
    
    - Add DLM_LOCK fuse opcode
    - Add cache page lock caching for writeback cache functionality.
    This means sending out a FUSE call whenever the kernel prepares a page
    for writeback cache. The kernel will manage the cache so that it will keep
    track of already acquired locks.
    (except for the case that is documented in the code)
    - Use rb-trees for the management of the already 'locked' page ranges
    - Use rw_semaphore for synchronization in fuse_dlm_cache
    
    (imported from commit 287c8840b60d5cdcf806b16e8cc5722f2dbf0738)

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -31,6 +31,7 @@
 #include <linux/pid_namespace.h>
 #include <linux/refcount.h>
 #include <linux/user_namespace.h>
+#include "fuse_dlm_cache.h"
 
 /** Default max number of pages that can be used in a single read request */
 #define FUSE_DEFAULT_MAX_PAGES_PER_REQ 32
@@ -113,6 +114,17 @@ struct fuse_backing {
 	struct rcu_head rcu;
 };
 
+/**
+ * data structure to save the information that we have
+ * requested dlm locks for the given area from the fuse server
+*/
+struct dlm_locked_area
+{
+	struct list_head list;
+	loff_t offset;
+	size_t size;
+};
+
 /** FUSE inode */
 struct fuse_inode {
 	/** Inode data */
@@ -168,6 +180,9 @@ struct fuse_inode {
 
 			/* waitq for direct-io completion */
 			wait_queue_head_t direct_io_waitq;
+
+			/* dlm locked areas we have sent lock requests for */
+			struct fuse_dlm_cache dlm_locked_areas;
 		};
 
 		/* readdir cache (directory only) */
@@ -908,6 +923,9 @@ struct fuse_conn {
 
 	/* Is statx not implemented by fs? */
 	unsigned int no_statx:1;
+
+	/* do we have support for dlm in the fs? */
+	unsigned int dlm:1;
 
 	/** Passthrough support for read/write IO */
 	unsigned int passthrough:1;
