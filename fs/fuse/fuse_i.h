2d6cfc032ad15ba27cb49d1ed4ee8af1b7877871
Bernd Schubert <bschubert@ddn.com>
2025-06-20 17:34:53 +0200

    fuse: add DLM_LOCK opcode
    When having writeback cache enabled it is beneficial for data consistency
    to communicate to the FUSE server when the kernel prepares a page for caching.
    This lets the FUSE server react and lock the page.
    
    Additionally the kernel lets the FUSE server decide how much data it locks by the
    same call and keeps the given information in the dlm lock management.
    
    If the feature is not supported it will be disabled after first unsuccessful use.
    
    - Add DLM_LOCK fuse opcode
    - Add cache page lock caching for writeback cache functionality.
    This means sending out a FUSE call whenever the kernel prepares a page
    for writeback cache. The kernel will manage the cache so that it will keep
    track of already acquired locks.
    (except for the case that is documented in the code)
    - Use rb-trees for the management of the already 'locked' page ranges
    - Use rw_semaphore for synchronization in fuse_dlm_cache
    
    (cherry picked from commit 287c8840b60d5cdcf806b16e8cc5722f2dbf0738)
    (cherry picked from commit 3b0ae3bcb6c2edf35b7d6993e1df52a33e21a41d)

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -31,6 +31,7 @@
 #include <linux/pid_namespace.h>
 #include <linux/refcount.h>
 #include <linux/user_namespace.h>
+#include "fuse_dlm_cache.h"
 
 /** Default max number of pages that can be used in a single read request */
 #define FUSE_DEFAULT_MAX_PAGES_PER_REQ 32
@@ -82,6 +83,17 @@ struct fuse_submount_lookup {
 
 	/** The request used for sending the FORGET message */
 	struct fuse_forget_link *forget;
+};
+
+/**
+ * data structure to save the information that we have
+ * requested dlm locks for the given area from the fuse server
+*/
+struct dlm_locked_area
+{
+	struct list_head list;
+	loff_t offset;
+	size_t size;
 };
 
 /** FUSE inode */
@@ -142,6 +154,9 @@ struct fuse_inode {
 
 			/* List of writepage requestst (pending or sent) */
 			struct rb_root writepages;
+
+			/* dlm locked areas we have sent lock requests for */
+			struct fuse_dlm_cache dlm_locked_areas;
 		};
 
 		/* readdir cache (directory only) */
@@ -840,6 +855,9 @@ struct fuse_conn {
 
 	/* Is statx not implemented by fs? */
 	unsigned int no_statx:1;
+
+	/* do we have support for dlm in the fs? */
+	unsigned int dlm:1;
 
 	/* Use io_uring for communication */
 	unsigned int io_uring;
