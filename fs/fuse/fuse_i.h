23287994f2134d7c30c510f87d6f4f76bb6e3a10
Horst Birthelmer <hbirthelmer@ddn.com>
2025-09-16 13:31:45 +0200

    fuse: add compound command to combine multiple requests
    fuse.h: add new opcode FUSE_COMPOUND
    
    fuse_compound.c: add new functionality to pack multiple
    fuse operations into one compound command
    
    file.c: add an implementation of open+getattr
    
    Signed-off-by: Horst Birthelmer <hbirthelmer@ddn.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -866,6 +866,9 @@ struct fuse_conn {
 	/* Use io_uring for communication */
 	unsigned int io_uring;
 
+	/* Does the filesystem support compound operations? */
+	unsigned int compound_open_getattr:1;
+
 	/** The number of requests waiting for completion */
 	atomic_t num_waiting;
 
@@ -1192,8 +1195,22 @@ void __exit fuse_ctl_cleanup(void);
  * Simple request sending that does request allocation and freeing
  */
 ssize_t fuse_simple_request(struct fuse_mount *fm, struct fuse_args *args);
+ssize_t fuse_compound_request(struct fuse_mount *fm, struct fuse_args *args);
 int fuse_simple_background(struct fuse_mount *fm, struct fuse_args *args,
 			   gfp_t gfp_flags);
+
+/**
+ * Compound request API
+ */
+struct fuse_compound_req;
+
+struct fuse_compound_req *fuse_compound_alloc(struct fuse_mount *fm, uint32_t flags);
+int fuse_compound_add(struct fuse_compound_req *compound,
+		    struct fuse_args *args);
+ssize_t fuse_compound_send(struct fuse_compound_req *compound);
+int fuse_compound_get_error(struct fuse_compound_req * compound,
+			int op_idx);
+void fuse_compound_free(struct fuse_compound_req *compound);
 
 /**
  * End a finished request
@@ -1447,9 +1464,11 @@ void fuse_file_io_release(struct fuse_file *ff, struct inode *inode);
 
 /* file.c */
 struct fuse_file *fuse_file_open(struct fuse_mount *fm, u64 nodeid,
-				 unsigned int open_flags, bool isdir);
+								struct inode *inode,
+								unsigned int open_flags, bool isdir);
 void fuse_file_release(struct inode *inode, struct fuse_file *ff,
 		       unsigned int open_flags, fl_owner_t id, bool isdir);
+
 #ifdef CONFIG_MIGRATION
 int fuse_migrate_folio(struct address_space *mapping, struct folio *dst,
 		struct folio *src, enum migrate_mode mode);
