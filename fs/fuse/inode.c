0ea57a5496221f6b92de777b72953cccd6bacc0b
Horst Birthelmer <hbirthelmer@ddn.com>
2026-06-22 08:54:19 +0200

    fuse: drop BDI_CAP_STRICTLIMIT from fuse bdi setup
    Signed-off-by: Horst Birthelmer <hbirthelmer@ddn.com>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -1834,7 +1834,7 @@ static int fuse_bdi_init(struct fuse_conn *fc, struct super_block *sb)
 
 	/* fuse does it's own writeback accounting */
 	sb->s_bdi->capabilities &= ~BDI_CAP_WRITEBACK_ACCT;
-	sb->s_bdi->capabilities |= BDI_CAP_STRICTLIMIT;
+	sb->s_bdi->capabilities &= ~BDI_CAP_STRICTLIMIT;
 
 	/*
 	 * For a single fuse filesystem use max 1% of dirty +
